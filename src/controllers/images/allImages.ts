32b20e87ed991cffdaab69992b839c5c3f85d2f7
Kevengg <kevinandre.engell@gmail.com>
2023-11-07 12:35:17 +0100

    updated all images endpoint

--- src/controllers/images/allImages.ts
+++ src/controllers/images/allImages.ts
@@ -1,7 +1,7 @@
-import { ImageLink } from "@prisma/client";
 import { Controller, Get, Route } from "tsoa";
 import ErrorReturn from "../../moddels/errorReturn";
 import { db } from "../../db";
+import ImageLink from "../../moddels/imgLink";
 
 @Route("/images")
 export class AllImages extends Controller {
@@ -9,6 +9,6 @@ export class AllImages extends Controller {
     public async allImages(): Promise<ImageLink[] | ErrorReturn> {
         const images = await db.imageLink.findMany();
 
-        return images;
+        return images as unknown[] as ImageLink[];
     }
 }
